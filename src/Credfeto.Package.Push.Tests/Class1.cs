f17afd286e55b8588f4d7e143e2e6efdec9ee73a
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-13 18:22:01 +0000

    Added tests

---
+++ src/Credfeto.Package.Push.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using FunFair.Test.Common;
+using Xunit;
+
+namespace Credfeto.Package.Push.Tests;
+
+public sealed class PlaceholderTests : TestBase
+{
+    [Fact]
+    public void Placeholder()
+    {
+        Assert.True(true, "Need to refactor the main package to ake it testable");
+    }
+}
