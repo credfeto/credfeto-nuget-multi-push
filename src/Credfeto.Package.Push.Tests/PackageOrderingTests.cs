2a819b09b883ef64bf3ef768dae6ff1ff1f35f6d
Mark Ridgwell <273118822+dnyw4l3n13@users.noreply.github.com>
2026-05-12 04:18:18 +0100

    feat: add unit tests for package ordering, naming, path helpers and retry delay calculator
    Closes #170

---
+++ src/Credfeto.Package.Push.Tests/PackageOrderingTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Credfeto.Package.Push.Helpers;
+using FunFair.Test.Common;
+using Xunit;
+
+namespace Credfeto.Package.Push.Tests;
+
+public sealed class PackageOrderingTests : TestBase
+{
+    [Theory]
+    [InlineData("FunFair.all.1.2.3.nupkg")]
+    [InlineData("FunFair.ALL.1.2.3.nupkg")]
+    [InlineData("FunFair.All.4.5.6.nupkg")]
+    public void IsMetaPackageForMetaPackageReturnsTrue(string packageId)
+    {
+        Assert.True(PackageOrdering.IsMetaPackage(packageId), $"{packageId} should be identified as a meta-package");
+    }
+
+    [Theory]
+    [InlineData("FunFair.Common.1.2.3.nupkg")]
+    [InlineData("FunFair.nupkg")]
+    [InlineData("1.2.3.nupkg")]
+    [InlineData("FunFair.all.nupkg")]
+    public void IsMetaPackageForNonMetaPackageReturnsFalse(string packageId)
+    {
+        Assert.False(
+            PackageOrdering.IsMetaPackage(packageId),
+            $"{packageId} should not be identified as a meta-package"
+        );
+    }
+
+    [Theory]
+    [InlineData("all")]
+    [InlineData("ALL")]
+    [InlineData("All")]
+    public void IsAllTagForAllTagReturnsTrue(string value)
+    {
+        Assert.True(PackageOrdering.IsAllTag(value.AsSpan()), $"\"{value}\" should be identified as the all tag");
+    }
+
+    [Theory]
+    [InlineData("other")]
+    [InlineData("allx")]
+    [InlineData("")]
+    public void IsAllTagForNonAllTagReturnsFalse(string value)
+    {
+        Assert.False(PackageOrdering.IsAllTag(value.AsSpan()), $"\"{value}\" should not be identified as the all tag");
+    }
+}
